0213aa5f65ad01503cd71bc7e47eb92870f62f80
emmanueles1993 <emmanueles1993@hotmail.com>
2020-02-05 18:58:27 -0500

    point

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
-#include< stdio.h>
+#include <stdio.h>
+
+/**
+ * main - prints a string using puts
+ *
+ * Return: 0 (Success)
+ */
+
 int main(void)
 {
-   char string [] = ""Programming is like building a multilingual puzzle!";
-  puts (string)
-
-   
-	return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
