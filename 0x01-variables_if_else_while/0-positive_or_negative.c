162080478c02d1fcfb2ec932aaa7adfb7393b68b
emmanueles1993 <emmanueles1993@hotmail.com>
2020-02-06 16:49:10 -0500

    command  to zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,7 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+
 int main(void)
 {
 int n;
@@ -19,9 +20,9 @@ else if (n == 0)
 {
 printf("%d is zero\n", n);
 }
-else if (n < O)
+else if (n < 0)
 {
 printf("%d is negative\n", n);
 }
 return (0);
-}
+
