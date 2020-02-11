bb5a6ebf31956c5ec2e7cdfd9ce059a68bd108c1
emmanueles1993 <emmanueles1993@hotmail.com>
2020-02-10 19:16:25 -0500

    version 1

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char hbtn[] = "Holberton";
+int d = 0;
+
+for (hbtn[d] = hbtn[d]; hbtn[d]; d++)
+_putchar(hbtn[d]);
+_putchar('\n');
+return (0);
+}
