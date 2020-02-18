7cba95c5100e9923b48f84a726d4e3b376120f5e
emmanueles1993 <emmanueles1993@hotmail.com>
2020-02-18 17:56:28 -0500

    version finished

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,12 +4,15 @@
  *@s: is variable
  * Return: Always 0.
  */
-void print_rev(char *s);
+void print_rev(char *s)
 {
 int a;
 int b;
-for (a = 0; str[a] != '\0'; a++)
+for (a = 0; s[a] != '\0'; a++)
 {}
-for (b = a - 1; b >=0; b--)
+for (b = a - 1; b >= 0; b--)
 {
-   
+_putchar(s[b]);
+}
+_putchar('\n');
+}
